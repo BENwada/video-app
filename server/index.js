a0d821039d9e9aa0274db2c69230fef0bab8b182
BENwada <domo0103wada@gmail.com>
2022-07-19 18:58:33 +0900

    auth add

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import commentRoutes from "./routes/comments.js";
+import videoRoutes from "./routes/videos.js";
 
 const app = express();
 dotenv.config();
@@ -15,6 +19,22 @@ const connect = () => {
       throw err;
     });
 };
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/videos", videoRoutes);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
 app.listen(8800, () => {
   connect();
